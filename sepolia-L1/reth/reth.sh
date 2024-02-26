7386a624984d43ce908a7fc3c39cda3e07114706
clabby <ben@clab.by>
2024-02-26 23:15:31 +0000

    Sepolia
    x

--- sepolia-L1/reth/reth.sh
+++ sepolia-L1/reth/reth.sh
@@ -2,15 +2,14 @@
 
 /usr/local/bin/reth node \
   --datadir /db \
-  --chain base \
-  --metrics 0.0.0.0:9430 \
+  --chain sepolia \
+  --metrics 0.0.0.0:9421 \
   --http \
-  --http.port 9545 \
+  --http.port 8546 \
   --http.addr 0.0.0.0 \
   --http.api debug,eth,net,trace,txpool,rpc,web3,admin \
   --authrpc.jwtsecret jwt.hex \
   --authrpc.addr 0.0.0.0 \
-  --authrpc.port 9551 \
-  --discovery.port 30403 \
-  --rollup.sequencer-http https://sequencer.base.org \
+  --authrpc.port 8552 \
+  --port 30304 \
   -vvv
