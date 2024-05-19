0030386bdb65a55ddea52af9e8a97abe6645d84c
clabby <ben@clab.by>
2024-05-19 23:07:22 +0000

    prometheus support

--- sepolia-l2/op-node/op-node.sh
+++ sepolia-l2/op-node/op-node.sh
@@ -3,7 +3,7 @@ set -eou
 
 exec op-node \
     --l1="https://sepolia-reth:8546" \
-    --l2="http://sepolia-op-reth:8547" \
+    --l2="http://sepolia-op-reth:8553" \
     --rpc.addr="0.0.0.0" \
     --rpc.port=9545 \
     --l2.jwt-secret=/db/jwttoken/jwt.hex \
