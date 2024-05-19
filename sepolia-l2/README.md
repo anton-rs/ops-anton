e3efe3988a031f7f423282b12f34ab8a7bac0dc1
refcell <abigger87@gmail.com>
2024-05-19 10:24:31 -0400

    feat: sepolia-l2

---
+++ sepolia-l2/README.md
@@ -0,0 +1,9 @@
+# `sepolia-l2`
+
+Anton's docker environment for the L2 clients (`reth` + `op-node`).
+
+## Usage
+
+To start the L2 clients: `make l2_sepolia_up`
+
+To stop the L2 clients and purge their images: `make l2_sepolia_down`
---
+++ sepolia-l2/reth/reth.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+/usr/local/bin/op-reth node \
+  --datadir /db \
+  --chain op-sepolia \
+  --metrics 0.0.0.0:9421 \
+  --http \
+  --http.port 8546 \
+  --http.addr 0.0.0.0 \
+  --http.api debug,eth,net,trace,txpool,rpc,web3,admin \
+  --authrpc.jwtsecret jwt.hex \
+  --authrpc.addr 0.0.0.0 \
+  --authrpc.port 8552 \
+  --port 30304 \
+  -vvv
